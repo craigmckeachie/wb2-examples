a8232ea9c4945e2ca1c1e6ac0dd8769a5db6a1a9
Craig McKeachie <craigdmckeachie@gmail.com>
2026-04-15 10:48:45 -0400

    example: Basic while loop

---
+++ src/main/java/com/pluralsight/WhileExample.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class WhileExample {
+
+    public static void main(String[] args) {
+        int count = 1;
+
+        while (count <= 10) {
+            System.out.println(count + ". Eat Doughnuts");
+            count = count + 1;
+        }
+
+
+    }
+}

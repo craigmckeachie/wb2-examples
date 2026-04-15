260462136435c6a08e3f8f0e8b38ac32c4625684
Craig McKeachie <craigdmckeachie@gmail.com>
2026-04-15 14:38:32 -0400

    example: Number array

---
+++ src/main/java/com/pluralsight/NumberArray.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class NumberArray {
+
+    public static void main(String[] args) {
+        int[] numbers = {12,56,43,90,113};
+
+        for (int index = 0; index < numbers.length; index++) {
+            int number = numbers[index];
+            System.out.println(number);
+        }
+
+    }
+}

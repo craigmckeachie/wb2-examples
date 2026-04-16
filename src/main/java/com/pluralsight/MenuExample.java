768485ac1be04994905d9e5c85ccdec2fee269db
Craig McKeachie <craigdmckeachie@gmail.com>
2026-04-16 12:30:41 -0400

    example: simple menu

---
+++ src/main/java/com/pluralsight/MenuExample.java
@@ -0,0 +1,54 @@
+package com.pluralsight;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class MenuExample {
+    static Scanner input = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+        boolean programIsRunning = true;
+
+
+        while (programIsRunning){
+            System.out.print("""
+                            What do you want to do?
+                             1 - First Screen
+                             2 - Second Screen
+                             3 - Quit
+                            Enter your command: """);
+
+
+            int command = input.nextInt();
+            
+            if (command == 1){
+                runFirstScreen();
+            } else if (command == 2) {
+                runSecondScreen();
+            } else if (command== 3) {
+                programIsRunning = false;
+            }else{
+                System.out.println("Invalid command try again.");
+            }
+
+        }
+
+    }
+
+    private static void runFirstScreen(){
+        System.out.println("First Screen");
+        System.out.println("-------------");
+        System.out.println();
+        System.out.println();
+    }
+
+    private static void runSecondScreen(){
+        System.out.println("Second Screen");
+        System.out.println("-------------");
+        System.out.println();
+        System.out.println();
+    }
+
+
+}
